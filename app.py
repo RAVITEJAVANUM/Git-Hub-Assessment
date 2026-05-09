c00b10a47063e6273d067e5f947c6fea0265bfab
RAVITEJAVANUM <61192117+RAVITEJAVANUM@users.noreply.github.com>
2026-05-09 16:48:52 +0530

    Delete app.py

--- app.py
+++
@@ -1,4 +0,0 @@
-print("My github assessment")
-print("Added new feature to my project")
-print("Added additional functionality to the previous added feature")
-print("Added Feature-2 to my project")
